32a8c2f686ccca07b1d9f9ab3c74fd627965bdca
Natasha A. Hull <musicalnat@gmail.com>
2013-02-09 16:15:14 -0800

    Last Edits

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -60,7 +60,7 @@ class TopicsController < ApplicationController
 
     respond_to do |format|
       if @topic.update_attributes(params[:topic])
-        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
+        format.html { redirect_to topics_path, notice: 'Topic was successfully created.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,9 @@
 class Topic < ActiveRecord::Base
   attr_accessible :description, :title
   has_many :votes
+  
+  validates_presence_of :title
+  
+  validates_presence_of :description
+  
 end
