f8503035490edc6839fec1523dad09ff993c1a6a
Natasha A. Hull <musicalnat@gmail.com>
2013-03-28 04:42:57 -0700

    Added new features

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
   # GET /topics
   # GET /topics.json
   def index
-    @topics = Topic.all
+    @topics = Topic.all.sort_by {|topic| topic.title.downcase}
     @ip = request.remote_ip
 
     respond_to do |format|
@@ -125,7 +125,7 @@ class TopicsController < ApplicationController
   end
   
   def compare
-    @topics = Topic.all
+    @topics = Topic.all.sort_by {|topic| topic.title.downcase}
     @ip = request.remote_ip
 
     respond_to do |format|
