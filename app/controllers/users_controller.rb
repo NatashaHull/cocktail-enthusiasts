0d5a9db5fa7c8220327be9dc69f891acf8a70af5
Natasha Hull <musicalnat@gmail.com>
2013-09-14 00:25:22 -0700

    Added ability to change your password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user && @user.authenticate(params[:user][:old_password]) && params[:user][:password] == params[:user][:password_confirmation]
+      @user.update_attributes(name: @user.name, password: params[:user][:password], password_confirmation: params[:user][:password_confirmation])
+      redirect_to topics_path
+    else
+      flash[:error] = "The user information you submitted is invalid."
+      render 'edit'
+    end
+  end
+
   def login
     user = User.find_by_name(params[:name][:name])
     if user && user.authenticate(params[:password][:password])
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :topics, :votes, :comments, :password, :password_confirmation
+  attr_accessible :name, :topics, :votes, :comments, :old_password, :password, :password_confirmation
   
   has_many :topics
   has_many :votes
